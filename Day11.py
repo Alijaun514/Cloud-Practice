fafb90fe4a17e37b0907e9f9113601964a14adfc
alijaun514 <muhammadalijaun0514@gmaile.com>
2026-02-27 07:57:41 -0930

    for loop

---
+++ Day11.py
@@ -0,0 +1,9 @@
+# For loop 
+for student in ["ali", "Qasim", "kumail", "sadaf", "Fatima"]: # list of student
+    print(student)
+    
+# for user in "we are learning for loop": # user string
+#      print(user)  
+
+#      for user_details in {"name": "ali", "age": "17", "job": "xyz", "city": "khanewal"}: # dictionary
+#         print(user_details) 

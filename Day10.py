16ff21291db5c9a204774c598f5500e271ec1626
Alijaun514 <muhammadalijaun0514@gmail.com>
2026-03-03 14:02:19 -0800

    while condition

---
+++ Day10.py
@@ -0,0 +1,13 @@
+# num = 5
+# while  num < 15:
+#     print("loop working time" , num)
+#     num = num + 1
+
+# user = 15
+# while user > 12:
+#     print("loop execution time" ,user)
+#     user = user + 1
+value = 0
+while value < 3:
+    print("number time", value)
+    value = value + 1

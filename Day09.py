91c6b658f09dbec58faa8d419287d0b9a345df0c
Alijaun514 <muhammadalijaun0514@gmail.com>
2026-03-04 13:18:58 -0800

    if elif else

---
+++ Day09.py
@@ -0,0 +1,22 @@
+# marka = int(input("Enter marks = "))
+# if marka >= 90:
+#     print("The Grade is A ")
+# elif marka >= 75:
+#     print("grade is B ")
+# elif marka >= 50:
+#     print("grade is C ")
+# else:
+#     print("Fail")        
+num = int(input("chose a number = "))
+if num >= 5:
+    print("colour is GREEN")
+elif num >= 10:
+    print("colour is RED")
+elif num >= 15:
+    print("colour is BLUE")    
+elif num >= 20:
+    print("colour is WHITE")    
+elif num >= 25:
+    print("colour is BLACK")
+else:
+    print("N O COLOUR HAS BEEN DETECDED")        

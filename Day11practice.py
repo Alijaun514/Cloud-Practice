ab46bde3e3bca32b337bd711e09d9d9bccb59b87
Alijaun514 <muhammadalijaun0514@gmail.com>
2026-03-06 13:43:52 -0800

    range using for loop

---
+++ Day11practice.py
@@ -0,0 +1,14 @@
+user = {"name" : "Ali", "class": "v1" }
+for user in user:
+    print(user)
+
+print(list(range(0,12)))
+
+for i in range(6):
+    print(i)
+
+for seq in range(2 , 24, 3):
+    print(seq)
+
+seq = list(range(3 , 66, 6))
+print(seq)
